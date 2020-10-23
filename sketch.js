ac67de053311f79a1010100884501802c385ba43
Adityalio <69355632+Adityalio@users.noreply.github.com>
2020-10-23 20:03:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//Create variables here
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+  createCanvas(800, 700);
+  
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
